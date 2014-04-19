f7bb4e20f35b51f58e4f48c0957ec11deba00abd
minh <minh@MacBook-Pro-cua-minh.local>
2014-04-19 18:06:11 +0000

    them slide

--- Resources/app.js
+++ Resources/app.js
@@ -7,15 +7,12 @@ if (Ti.version < 1.8) {
 	new (require('ui-controller/AllData'));
 
 	// var Window = new (require('ui/WindowMain'))();
-<<<<<<< HEAD
 	// Window.open();
 	// var Window=new(require('ui/DangNhap'))();
 	// Window.open();
 	var home=new (require('/ui/slide_menu'))(1);
 	home.open();
-=======
 	// Window.open();
-	var Window=new(require('ui/DangNhap'))();
-	Window.open();
->>>>>>> 2763f51e1a5623377316ca34b35bea6021aa2fa4
+	// var Window=new(require('ui/DangNhap'))();
+	// Window.open();
 })();
