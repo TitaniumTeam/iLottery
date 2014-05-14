10de5d0fd6c0e689ece835a5549c2754176608da
minh <minh@MacBook-Pro-cua-minh.local>
2014-05-14 10:11:36 +0000

    fix all

--- Resources/app.js
+++ Resources/app.js
@@ -5,11 +5,11 @@ if (Ti.version < 1.8) {
 //(function() {
 new (require('ui-controller/AllData'));
 if (Ti.Platform.osname == "iphone") {
-	var home = new (require('/ui/slide_menu'))();
+	var home = new (require('/ui_bongda/slide_menu'))();
 	home.open();
 } else {
 	if (Ti.Platform.osname == "android") {
-		var home_android = new (require('ui/slide_menu_android'))();
+		var home_android = new (require('ui_bongda/slide_menu_android'))();
 		home_android.open();
 	}
 }
