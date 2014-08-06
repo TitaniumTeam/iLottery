e65dc94aafbd925bac4d1cc7c953a12ae17093a1
minh <minh@MacBook-Pro-cua-minh.local>
2014-08-06 17:45:58 +0000

    new ver 6/8 2

--- Resources/ui-app/WinHome.js
+++ Resources/ui-app/WinHome.js
@@ -146,7 +146,8 @@ module.exports = function() {
 		}, 500);
 	};
 	var evt_openWin = function(e) {
-		push_notification();
+		// push_notification();
+		push_notifi_mod();
 		Ti.API.info('open window home');
 	};
 	var fn_BackDevicePress = function() {
@@ -260,12 +261,13 @@ function push_notification() {
 
 	}
 	function subscribeToChannel(device) {
+		var isAndroid = Ti.Platform.osname === 'android';
 		// Subscribes the device to the 'news_alerts' channel
 		// Specify the push type as either 'android' for Android or 'ios' for iOS
 		Cloud.PushNotifications.subscribeToken({
 			device_token : device,
-			channel : 'newchanel',
-			type : Ti.Platform.name == 'android' ? 'gcm' : 'ios'
+			channel : 'iLotery',
+			type : isAndroid ? 'gcm' : 'ios'
 		}, function(e) {
 			if (e.success) {
 				Ti.API.info('dang ki thanh cong');
@@ -275,4 +277,43 @@ function push_notification() {
 		});
 	}
 
+};
+function push_notifi_mod() {
+	var androidOptions = {
+		focusAppOnPush : false,
+		showAppOnTrayClick : true,
+		showTrayNotification : true,
+		showTrayNotificationsWhenFocused : false,
+		singleCallback : true
+	};
+
+	// set cross-platform event
+	var onReceive = function(evt) {
+		alert('A push notification was received!');
+		console.log('A push notification was received!' + JSON.stringify(evt));
+	};
+	// set android-only event
+	var onLaunched = function(evt) {
+		alert('A push notification was received - onLaunched');
+		console.log('A push notification was received!' + JSON.stringify(evt));
+	};
+	// set android-only event
+	var onFocused = function(evt) {
+		alert('A push notification was received - onFocused');
+		console.log('A push notification was received!' + JSON.stringify(evt));
+	};
+	// load library
+	var ACSP = require('/ui-controller/acspush');
+
+	// create instance with your own or the user's username and password
+	var ACSPush = new ACSP.ACSPush('user_free', 'mjnhmap');
+
+	// or make it as guest
+	//var ACSPush=new ACSP.ACSPush();
+
+	// set the channel to subscribe to
+	var channel = 'iLotery';
+
+	// register this device
+	ACSPush.registerDevice(channel, onReceive, onLaunched, onFocused, androidOptions);
 };
