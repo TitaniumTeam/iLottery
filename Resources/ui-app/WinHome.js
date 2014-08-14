3b6edd336b38535e88583c338dbe6933d100fc09
minh <minh@MacBook-Pro-cua-minh.local>
2014-08-14 17:19:46 +0000

    new ver 14/8

--- Resources/ui-app/WinHome.js
+++ Resources/ui-app/WinHome.js
@@ -1,9 +1,8 @@
 module.exports = function() {
 	var db = Ti.Database.open('userinfo');
-	var newdb = Ti.Database.install('/assets/database/serviceinfo', 'serviceinfo');
+	var newdb = Ti.Database.install('/assets/database/servicedb', 'servicedb');
 	Ti.API.info('du lieu menu cap 1' + (newdb.execute("SELECT * FROM Menucap1_xoso").getRowCount()));
 	db.execute('CREATE TABLE IF NOT EXISTS SaveInfo(username TEXT PRIMARY KEY, password TEXT,type INTERGER,balance INTERGER);');
-	db.execute('CREATE TABLE IF NOT EXISTS TGHT(NGAY TEXT)');
 	var userinfo = db.execute("SELECT * FROM SaveInfo");
 	if (userinfo.isValidRow()) {
 		Ti.API.info('du lieu user:' + userinfo.getRowCount() + userinfo.fieldByName("username") + "/" + userinfo.fieldByName("type") + "/" + userinfo.fieldByName("balance"));
--- Resources/ui-bongda/VTuVan.js
+++ Resources/ui-bongda/VTuVan.js
@@ -60,25 +60,24 @@ function removeSK(sv) {
 		// if (sv.fu.evt_tblrow_click != null && sv.fu.evt_tblrow_click != undefined) {
 		// sv.ui.tbl1.removeEventListener('click', sv.fu.evt_tblrow_click);
 		// sv.ui.opt_dialog.removeEventListener('click', sv.fu.evt_optdialog);
-		Ti.API.info('remove su kien tu van xoso');
+		Ti.API.info('remove su kien tu van bongda');
 		// }
 
 	};
 };
 function tao_sukien(sv) {
-	var db_service = Ti.Database.open('serviceinfo');
+	var db_service = Ti.Database.open('servicedb');
 	var db_menucap1 = db_service.execute("SELECT * FROM Menucap1_bongda");
 	var db_menucap2 = db_service.execute("SELECT * FROM Menucap2_bongda");
-	var db_menucap3 = db_service.execute("SELECT * FROM Menucap3_bongda");
 	var dv_cap1 = [];
 	var dv_cap2 = [];
-	var dv_cap3 = [];
 	dv_cap1 = get_data_fromdb(db_menucap1);
 	dv_cap2 = get_data_fromdb(db_menucap2);
-	dv_cap3 = get_data_fromdb(db_menucap3);
+
 	// for (var i = 0; i < (dv_cap2.length); i++) {
-	// Ti.API.info('name:' + dv_cap2[i].name + "/id:" + dv_cap2[i].id + dv_cap2[i].act);
+		// Ti.API.info('name:' + dv_cap2[i].name + "/id:" + dv_cap2[i].id + dv_cap2[i].act);
 	// }
+
 	for (var i = 0; i < (dv_cap1.length); i++) {
 		sv.arr.rows[i] = Ti.UI.createTableViewRow({
 			width : Ti.App.size(640),
@@ -96,6 +95,7 @@ function tao_sukien(sv) {
 			idrow : i,
 			expanded : false,
 			backgroundSelectedImage : "/assets/icon/btn_tuvan_select.png",
+
 		});
 		sv.arr.Label_ten_dv[i] = Ti.UI.createLabel({
 			left : Ti.App.size(20),
@@ -110,158 +110,61 @@ function tao_sukien(sv) {
 			text : dv_cap1[i].name,
 			touchEnabled : false
 		});
-		sv.arr.ViewBack[i] = Ti.UI.createView({
-			left : 0,
-			top : Ti.App.size(90),
-			width : Ti.App.size(640),
-			layout : "vertical",
-		});
 		sv.arr.View_rows[i].add(sv.arr.Label_ten_dv[i]);
 		sv.arr.rows[i].add(sv.arr.View_rows[i]);
-		sv.arr.rows[i].add(sv.arr.ViewBack[i]);
 	}
 	sv.ui.tbl1.setData(sv.arr.rows);
 	// tao_sukien(sv);
 	for (var i = 0; i < (dv_cap1.length); i++) {
 		sv.arr.View_rows[i].addEventListener('click', function(e) {
-			// Ti.API.info('click row thu' + e.source.idrow);
-			if (e.source.expanded) {
-				e.source.expanded = false;
-				sv.arr.rows[e.source.idrow].setHeight(Ti.App.size(90));
-				sv.arr.ViewBack[e.source.idrow].removeAllChildren();
-				for (var j = 0; j < (dv_cap1.length); j++) {
-					if (j != (e.source.idrow)) {
-						sv.arr.ViewBack[j].removeAllChildren();
-						sv.arr.View_rows[j].expanded = false;
-						sv.arr.rows[j].setHeight(Ti.App.size(90));
-					}
-				}
-			} else {
-				// var dem = 0;
-				e.source.expanded = true;
-				sv.arr.rows[e.source.idrow].setHeight(Ti.App.size(90 * 4));
-				sv.arr.ViewBack[e.source.idrow].setHeight(Ti.App.size(90 * 3));
-				for (var j = 0; j < (dv_cap1.length); j++) {
-					if (j != (e.source.idrow)) {
-						if (Ti.Platform.osname != 'android')
-							sv.arr.ViewBack[j].removeAllChildren();
-						sv.arr.View_rows[j].expanded = false;
-						sv.arr.rows[j].setHeight(Ti.App.size(90));
-					}
-				}
-				for (var k = 0; k < (dv_cap2.length); k++) {
-					if (dv_cap2[k].parentid == e.source.id) {
-						// dem++;
-						sv.arr.ViewChuaLbl[k] = Titanium.UI.createView({
-							width : Ti.App.size(640),
-							height : Ti.App.size(90),
-							left : 0,
-							_name_menucap2 : dv_cap2[k].name,
-							_id_menucap2 : dv_cap2[k].id,
-							_action : dv_cap2[k].act ? dv_cap2[k].act : null,
-							_params : dv_cap2[k].thamso ? dv_cap2[k].thamso : null,
-							_servicenumber : dv_cap2[k].dauso ? dv_cap2[k].dauso : null,
-							_price : dv_cap2[k].price ? dv_cap2[k].price : null,
-							backgroundSelectedColor : Ti.App.Color.magenta,
-							backgroundColor : Ti.App.Color.nauden
-						});
-						sv.arr.ViewChuaLbl[k].add(Ti.UI.createView({
-							width : Ti.App.size(640),
-							height : Ti.App.size(2),
-							backgroundColor : "#2b2b2b",
-							bottom : 0,
-							touchEnabled : false,
-							left : 0
-						}));
-						sv.arr.Label_dvcon[k] = Ti.UI.createLabel({
-							left : Ti.App.size(100),
-							width : Ti.App.size(620),
-							textAlign : "left",
-							color : Ti.App.Color.superwhite,
-							height : Ti.App.size(90),
-							font : {
-								fontSize : Ti.App.size(30)
-							},
-							backgroundColor : "transparent",
-							text : dv_cap2[k].name.toString(),
-							touchEnabled : false
-						});
-						sv.arr.ViewChuaLbl[k].add(sv.arr.Label_dvcon[k]);
-						sv.arr.ViewBack[e.source.idrow].add(sv.arr.ViewChuaLbl[k]);
+			var menucap2 = {};
+			menucap2.id = [];
+			menucap2.name = [];
+			menucap2.act = [];
+			menucap2.param = [];
+			menucap2.ser_num = [];
+			menucap2.price = [];
+			for (var k = 0; k < (dv_cap2.length); k++) {
+				if (dv_cap2[k].parentid == e.source.id) {
+					menucap2.name.push(dv_cap2[k].name);
+					menucap2.id.push(dv_cap2[k].id);
+					menucap2.act.push(dv_cap2[k].act);
+					menucap2.param.push(dv_cap2[k].thamso);
+					menucap2.ser_num.push(dv_cap2[k].dauso);
+					menucap2.price.push(dv_cap2[k].price);
 
-						sv.ui.tbl1.setData(sv.arr.rows);
-						// sv.ui.tbl1.insertRowAfter(e.source.idrow,sv.arr.ViewChuaLbl[k]);
-						sv.arr.ViewChuaLbl[k].addEventListener('click', function(e) {
-							Ti.API.info('action****' + e.source._action);
-							if (e.source._action != null) {
-								if (Ti.Network.networkType == Ti.Network.NETWORK_NONE || Ti.Network.networkType == Ti.Network.NETWORK_UNKNOWN) {
-									var pop_upsms = new (require('/ui-user/PopUpSmsOff'))(e.source._servicenumber, e.source._action + " " + e.source._params, "DỊCH VỤ SX " + e.source._action + " " + e.source._name_menucap2);
-									pop_upsms.open({
-										modal : Ti.Platform.osname == 'android' ? true : false
-									});
-								} else {
-									tuvan_soxo({
-										"command" : e.source._action,
-										"param" : e.source._params,
-										"price" : e.source._price,
-									});
-								}
-
-							} else {
-								var name_menucap3 = [];
-								var id_menucap3 = [];
-								var isAndroid = Ti.Platform.osname === 'android';
-								for ( z = 0; z < (dv_cap3.length); z++) {
-									if (e.source._id_menucap2 == dv_cap3[z].parentid) {
-										name_menucap3.push(dv_cap3[z].name);
-										id_menucap3.push(dv_cap3[z].id);
-									}
-								}
-								if (!isAndroid) {
-									name_menucap3.push("Thoát");
-								}
-								var opt = Ti.UI.createOptionDialog({
-									buttonNames : ["Thoát"],
-									options : name_menucap3,
-									opaquebackground : true,
-									title : "Lựa chọn các tỉnh thành",
-									_action : e.source._action
-								});
-								opt.show();
-								opt.addEventListener('click', function(e) {
-									if (e.button || opt.getOptions().toString == 'Thoát') {
-										opt.hide();
-									} else {
-										var _cmd = null;
-										var _param = null;
-										var _price = null;
-										for (var q = 0; q < (dv_cap3.length); q++) {
-											if (id_menucap3[e.index] == dv_cap3[q].id) {
-												_cmd = dv_cap3[q].act;
-												_param = dv_cap3[q].thamso;
-												_price = dv_cap3[q].dauso;
-											}
-										}
-										if (Ti.Network.networkType == Ti.Network.NETWORK_NONE || Ti.Network.networkType == Ti.Network.NETWORK_UNKNOWN) {
-											var pop_upsms = new (require('/ui-user/PopUpSmsOff'))(_price, _cmd + " " + _param, "DỊCH VỤ SX " + _cmd + " " + name_menucap3[e.index]);
-											pop_upsms.open({
-												modal : Ti.Platform.osname == 'android' ? true : false
-											});
-										} else {
-											tuvan_soxo({
-												"command" : _cmd,
-												"param" : _param,
-												"price" : _price ,
-											});
-										}
-
-									}
-								});
-							}
-						});
-					}
 				}
 			}
+			var isAndroid = Ti.Platform.osname === 'android';
+			if (!isAndroid)
+				menucap2.name.push("Thoát");
+			var opt = Ti.UI.createOptionDialog({
+				buttonNames : ["Thoát"],
+				options : menucap2.name,
+				opaquebackground : true,
+				title : "Lựa chọn các tỉnh thành",
+			});
+			if(menucap2.name.length>0)
+			opt.show();
+			opt.addEventListener('click', function(e) {
+				if (e.button || opt.getOptions().toString == 'Thoát') {
+					opt.hide();
+				} else {
+					Ti.API.info('id' + menucap2.act[e.index] + menucap2.name[e.index]);
+					if (Ti.Network.networkType == Ti.Network.NETWORK_NONE || Ti.Network.networkType == Ti.Network.NETWORK_UNKNOWN) {
+						var pop_upsms = new (require('/ui-user/PopUpSmsOff'))(menucap2.ser_num[e.index], menucap2.act[e.index] + " " + menucap2.param[e.index], "DỊCH VỤ SX " + menucap2.act[e.index] + " " + menucap2.name[e.index]);
+						pop_upsms.open({
+							modal : Ti.Platform.osname == 'android' ? true : false
+						});
+					} else {
+						tuvan_soxo({
+							"command" : menucap2.act[e.index],
+							"param" : menucap2.param[e.index],
+							"price" : menucap2.price[e.index],
+						});
+					}
+				}
+			});
 		});
 	};
 };
@@ -314,20 +217,12 @@ function get_menu(sv) {
 					menucap2.push(jsonResuilt.menus[i]);
 				}
 			}
-		for (var i = 0; i < (jsonResuilt.menus.length); i++) {
-			for (var j = 0; j < (menucap2.length); j++) {
-				if (jsonResuilt.menus[i].parentid == menucap2[j].id) {
-					menucap3.push(jsonResuilt.menus[i]);
-				}
-			}
-		};
 		sv.vari.user_info.close();
 		sv.vari.db.close();
-		var db_service = Ti.Database.open('serviceinfo');
+		var db_service = Ti.Database.open('servicedb');
 		if (Ti.Platform.osname != 'android') {
 			db_service.execute("DELETE FROM Menucap1_bongda");
 			db_service.execute("DELETE FROM Menucap2_bongda");
-			db_service.execute("DELETE FROM Menucap3_bongda");
 		}
 		for (var i = 0; i < (menucap1.length); i++) {
 			db_service.execute("INSERT OR IGNORE INTO Menucap1_bongda VALUES(?,?)", (menucap1[i].id), menucap1[i].name);
@@ -337,9 +232,6 @@ function get_menu(sv) {
 				db_service.execute("INSERT OR IGNORE INTO Menucap2_bongda VALUES(?,?,?,?,?,?,?)", (menucap2[i].id), menucap2[i].name, menucap2[i].action, menucap2[i].params, menucap2[i].servicenumber, menucap2[i].price, menucap2[i].parentid);
 			else
 				db_service.execute("INSERT OR IGNORE INTO Menucap2_bongda VALUES(?,?,?,?,?,?,?)", (menucap2[i].id), menucap2[i].name, "", "", "", "", menucap2[i].parentid);
-		}
-		for (var i = 0; i < (menucap3.length); i++) {
-			db_service.execute("INSERT OR IGNORE INTO Menucap3_bongda VALUES(?,?,?,?,?,?,?)", menucap3[i].id, menucap3[i].name, menucap3[i].action, menucap3[i].params, menucap3[i].servicenumber, menucap3[i].price, menucap3[i].parentid);
 		}
 		db_service.close();
 	};
